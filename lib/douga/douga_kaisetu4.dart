84d5a3c35ebda3b47073ab74a31a93ae81fdfa16
okatoku331 <okatoku331@gmail.com>
2024-05-19 21:16:12 +0900

    6: 2024.5.19 21:15 webview削除

--- lib/douga/douga_kaisetu4.dart
+++ lib/douga/douga_kaisetu4.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 import 'package:youtube_player_iframe/youtube_player_iframe.dart';
-
-//import '../src/connectivity_sample.dart';
-
-
-///
 class DougaKaisetu4 extends StatelessWidget {
-  ///
   const DougaKaisetu4({super.key});
 
   @override
@@ -23,41 +16,52 @@ class DougaKaisetu4 extends StatelessWidget {
           ),
         ),
       ),
-        body: SingleChildScrollView(
-          padding: const EdgeInsets.all(32),
-          child: SizedBox(
+        body: const SizedBox(
             height: 1000,
-            child: LayoutBuilder(
-              builder: (context, constraints) {
-                if (constraints.maxWidth < 600) {
-                  return const Column(
+            child: Column(
                     children: [
                       PlayerWidget(),
-                      Divider(),
-                      Expanded(child: _WebViewWidget()),
                     ],
-                  );
-                }
-
-                return const Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Expanded(child: PlayerWidget()),
-                    VerticalDivider(),
-                    Expanded(child: _WebViewWidget()),
-                  ],
-                );
-              },
+                  ),
             ),
+      persistentFooterButtons: <Widget>[
+        SizedBox(
+          height: 40,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                onPressed: () {
+                  // 動画を一時停止
+                 // _controller.pauseVideo();
+                },
+                icon: const Icon(Icons.pause),
+              ),
+              IconButton(
+                onPressed: () {
+                  // 動画を再生
+                  //_controller.playVideo();
+                },
+                icon: const Icon(Icons.play_arrow),
+              ),
+              IconButton(
+                onPressed: () {
+                  // 動画を最初から再生
+                 // _controller.stopVideo();
+                  Navigator.pop(context);
+                },
+                icon: const Icon(Icons.stop),
+              ),
+            ],
           ),
         ),
-      );
+      ],
+    );
   }
 }
 
-///
 class PlayerWidget extends StatelessWidget {
-  ///
   const PlayerWidget({super.key});
 
   @override
@@ -72,27 +76,5 @@ class PlayerWidget extends StatelessWidget {
         ),
         aspectRatio: 5 / 8,
       );
-  }
-}
-
-class _WebViewWidget extends StatefulWidget {
-  const _WebViewWidget();
-
-  @override
-  State<_WebViewWidget> createState() => _WebViewWidgetState();
-}
-
-class _WebViewWidgetState extends State<_WebViewWidget> {
-  @override
-  void initState() {
-    super.initState();
-    _controller = WebViewController()..loadRequest(Uri.https('okatoku331.net'));
-  }
-
-  late final WebViewController _controller;
-
-  @override
-  Widget build(BuildContext context) {
-    return WebViewWidget(controller: _controller);
   }
 }
