dbfed3a6af545b0cc73a010e195fb79f8fa2f470
oka-teruhiro <oka331@gmail.com>
2025-01-23 21:45:36 +0900

    23: 6.2.6 装飾を実装

--- lib/widgets/omikuji_result_screen.dart
+++ lib/widgets/omikuji_result_screen.dart
@@ -1,6 +1,7 @@
 // lib/widgets/omikuji_result_screen.dart を新規作成
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:win32/win32.dart';
 import 'dart:math' show pi;
 import '../models/omikuji.dart';              // 追加
 import '../utils/fortune_level_utils.dart';    // 追加
@@ -23,15 +24,23 @@ class _OmikujiResultScreenState extends State<OmikujiResultScreen>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   late Animation<double> _fadeAnimation;
+  late Animation<Offset> _slideAnimation;
 
   @override
   void initState() {
     super.initState();
     _controller = AnimationController(
-      duration: const Duration(seconds: 2),
+      duration: const Duration(seconds: 3),
       vsync: this,
     );
     _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(_controller);
+    _slideAnimation = Tween<Offset>(
+      begin: const Offset(0.0, 1.0),   // 画面下から開始
+      end: const Offset(0.0, 0.0),     // 画面の上から20%の位置で停止
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeOutCubic,
+    ));
     _controller.forward();
   }
 
@@ -44,81 +53,106 @@ class _OmikujiResultScreenState extends State<OmikujiResultScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          color: Colors.tealAccent,
-          onPressed: () => Navigator.of(context).pop(),
-        ),
-      ),
-      body: Stack(
-        children: [
-          Center(
-            child: FadeTransition(
-              opacity: _fadeAnimation,
-              child: Container(
-                margin: const EdgeInsets.all(24),
-                padding: const EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  color: Colors.black87,
-                  borderRadius: BorderRadius.circular(12),
-                  border: Border.all(
-                    color: Colors.tealAccent,
-                    width: 1,
-                  ),
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      FortuneLevelUtils.getFortuneLevelText(widget.omikuji.fortuneLevel),
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    ...widget.omikuji.content.map((line) => Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 4),
-                      child: Text(
-                        line,
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
+      backgroundColor: Colors.black87,
+      body: SlideTransition(
+        position: _slideAnimation,
+        child: Stack(
+          children: [
+            FrameDecoration(
+              width: (MediaQuery.of(context).size.width) * 1.0,
+              height: (MediaQuery.of(context).size.height) * 1.0,
+            ),
+            Center(
+              child: FadeTransition(
+                opacity: _fadeAnimation,
+                child: Container(
+                  margin: const EdgeInsets.all(0),
+                  padding: const EdgeInsets.all(0),
+
+
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+
+                      //const SizedBox(height: 20),
+                      ...widget.omikuji.content.map((line) => Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 0),
+                        child: Text(
+                          line,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                          ),
                         ),
-                      ),
-                    )),
-                  ],
+                      )),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          // コーナー装飾
-          const Positioned(
-            left: 0,
-            top: 0,
-            child: CornerDecoration(corner: 1),
-          ),
-          const Positioned(
-            right: 0,
-            top: 0,
-            child: CornerDecoration(corner: 2),
-          ),
-          const Positioned(
-            left: 0,
-            bottom: 0,
-            child: CornerDecoration(corner: 3),
-          ),
-          const Positioned(
-            right: 0,
-            bottom: 0,
-            child: CornerDecoration(corner: 4),
-          ),
-        ],
+            // コーナー装飾
+            const Positioned(
+              left: 0,
+              top: 60,
+              child: CornerDecoration(
+                  corner: 1,
+                  //style: 5
+              ), // スタイル5を使用
+            ),
+            const Positioned(
+              right: 0,
+              top: 60,
+              child: CornerDecoration(
+                  corner: 2,
+                  //style: 5
+              ),
+            ),
+            const Positioned(
+              left: 0,
+              bottom: 80,
+              child: CornerDecoration(
+                  corner: 4,
+                  //style: 6
+              ), // スタイル6を使用
+            ),
+            const Positioned(
+              right: 0,
+              bottom: 80,
+              child: CornerDecoration(
+                  corner: 3,
+                  //style: 6
+              ),
+            ),
+            // 戻るボタンを追加
+            Positioned(
+              bottom: 20,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: TextButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  style: TextButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 40,
+                      vertical: 12,
+                    ),
+                    side: const BorderSide(
+                      color: Colors.tealAccent,
+                      width: 1,
+                    ),
+                  ),
+                  child: const Text(
+                    '戻る',
+                    style: TextStyle(
+                      color: Colors.tealAccent,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -127,11 +161,13 @@ class _OmikujiResultScreenState extends State<OmikujiResultScreen>
 class CornerDecoration extends StatelessWidget {
   final double size;
   final int corner;
+  //final int style; // 5または6を指定
 
   const CornerDecoration({
     Key? key,
-    this.size = 50.0,
+    this.size = 80.0,
     required this.corner,
+    //required this.style,
   }) : super(key: key);
 
   @override
@@ -141,16 +177,30 @@ class CornerDecoration extends StatelessWidget {
       child: SvgPicture.string(
         '''
         <svg width="$size" height="$size" viewBox="0 0 100 100">
+          <!-- メインの渦巻きパターン -->
           <path
-            d="M 10 90 C 40 90, 40 90, 40 60 C 40 40, 40 40, 60 40 C 90 40, 90 40, 90 10"
-            stroke="#8B4513"
-            stroke-width="4"
+            d="M 10 100
+              L 10 30
+              L 30 30
+              L 30 10
+              L 100 10"
+            stroke="#64FFDA"
+            stroke-width="2"
+            
             fill="none"
           />
+          <!-- サブの渦巻き -->
           <path
-            d="M 20 80 C 35 80, 35 80, 35 65 C 35 50, 35 50, 50 50 C 80 50, 80 50, 80 20"
-            stroke="#8B4513"
-            stroke-width="4"
+            d="M 10 10
+               L 10 20
+               L 40 20
+               L 40 40
+               L 20 40
+               L 20 10
+               L 10 10"
+            stroke="#64FFDA"
+            stroke-width="2"
+            
             fill="none"
           />
         </svg>
@@ -158,6 +208,43 @@ class CornerDecoration extends StatelessWidget {
         width: size,
         height: size,
       ),
+    );
+  }
+}
+
+class FrameDecoration extends StatelessWidget {
+  final double width;
+  final double height;
+
+  const FrameDecoration({
+    Key? key,
+    required this.width,
+    required this.height,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SvgPicture.string(
+      '''
+      <svg width="$width" height="$height" viewBox="0 0 $width $height">
+        
+        <line
+          x1="120" y1="69"
+          x2="${width-120}" y2="69"
+          stroke="#64FFDA"
+          stroke-width="2"
+        />
+        <line
+          x1="120" y1="${height-88}"
+          x2="${width-120}" y2="${height-88}"
+          stroke="#64FFDA"
+          stroke-width="2"
+        />
+
+      </svg>
+      ''',
+      width: width,
+      height: height,
     );
   }
 }
