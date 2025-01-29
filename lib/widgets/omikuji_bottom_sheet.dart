8c518c9966963b1ce404f41e00fecd86ce4a45b3
oka-teruhiro <oka331@gmail.com>
2025-01-29 23:18:52 +0900

    28: 6.3.4 飾り枠をアニメ化

--- lib/widgets/omikuji_bottom_sheet.dart
+++ lib/widgets/omikuji_bottom_sheet.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -10,13 +12,14 @@ class OmikujiBottomSheet extends StatefulWidget {
 }
 
 class _OmikujiBottomSheetState extends State<OmikujiBottomSheet>
-  with SingleTickerProviderStateMixin {  // アニメーションに必要なミックスイン
+    with SingleTickerProviderStateMixin {
+  // アニメーションに必要なミックスイン
 
   late AnimationController _controller; // アニメーションのコントローラー
-  late Animation<double> _animation; // アニメーションの値
+  //late Animation<double> _animation; // アニメーションの値
   late Animation<double> _slideAnimation; // スライドアップ用
-  late Animation<double> _fadeAnimation; // フェードイン用
-
+  //late Animation<double> _fadeAnimation; // フェードイン用
+  late Animation<double> _pathAnimation; // パス描画用
 
   @override
   void initState() {
@@ -24,21 +27,32 @@ class _OmikujiBottomSheetState extends State<OmikujiBottomSheet>
 
     // コントローラーの設定
     _controller = AnimationController(
-      duration: const Duration(seconds: 2),// アニメーションの長さ
+      duration: const Duration(seconds: 5), // アニメーションの長さ
       vsync: this,
     );
 
     // スライドアップアニメーション(0-1秒)
     _slideAnimation = CurvedAnimation(
-        parent: _controller,
-        curve: const Interval(0.0, 0.5, curve: Curves.easeOut),
+      parent: _controller,
+      curve: const Interval(0.0, 0.2, curve: Curves.easeOut), // 最初の30%
     );
 
+    /*
     // フェードインアニメーション(1-2秒)
     _fadeAnimation = CurvedAnimation(
-        parent: _controller,
-        curve: const Interval(0.5, 1.0, curve: Curves.easeIn),
+      parent: _controller,
+      curve: const Interval(0.3, 1.0, curve: Curves.easeIn),
     );
+    */
+
+    // 一筆書きアニメーション(1秒-3秒)
+    _pathAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(
+        parent: _controller,
+        curve:const Interval(0.2, 1.0, curve: Curves.linear),
+    ));
 
     /*
     // アニメーションの設定
@@ -61,14 +75,17 @@ class _OmikujiBottomSheetState extends State<OmikujiBottomSheet>
 
   @override
   Widget build(BuildContext context) {
-
     double w0 = MediaQuery.of(context).size.width;
     double h0 = MediaQuery.of(context).size.height;
     double h1 = h0 * 0.9;
     double h2 = h0 * 0.1;
 
+    // パスのおおよその全長を計算（実際のパスに合わせて調整が必要）
+    //double pathLength = (w0 *2 + h1 * 2) *2;  // 外周の2倍程度を目安に
+    double pathLength = 2700;  // 実際のパスの長さより大きめの値を設定
 
-    return SizeTransition( // サイズ変更アニメーション
+    return SizeTransition(
+      // サイズ変更アニメーション
       sizeFactor: _slideAnimation,
       axis: Axis.vertical,
       child: SizedBox(
@@ -76,81 +93,103 @@ class _OmikujiBottomSheetState extends State<OmikujiBottomSheet>
         height: h1,
         child: Container(
           color: Colors.blueGrey,
-          child: FadeTransition(
-            opacity: _fadeAnimation,
-            child: SvgPicture.string(
-              '''
-      <svg width= w0 height= h1 viewBox="0 0 $w0 $h1">
-                  
-                  <path
-            d="M 10 100
-              L 10 50
-              L 30 50
-              L 30 10
-              L 40 10
-              L 40 20
-              L 10 20
-              L 10 10
-              L 20 10
-              L 20 40
-              L 10 40
-              L 10 30
-              L 50 30
-              L 50 10
-              L ${w0-50} 10
-              L ${w0-50} 30
-              L ${w0-10} 30
-              L ${w0-10} 40
-              L ${w0-20} 40
-              L ${w0-20} 10
-              L ${w0-10} 10
-              L ${w0-10} 20
-              L ${w0-40} 20
-              L ${w0-40} 10
-              L ${w0-30} 10
-              L ${w0-30} 50
-              L ${w0-10} 50
-              L ${w0-10} ${h1-50-h2}
-              L ${w0-30} ${h1-50-h2}
-              L ${w0-30} ${h1-10-h2}
-              L ${w0-40} ${h1-10-h2}
-              L ${w0-40} ${h1-20-h2}
-              L ${w0-10} ${h1-20-h2}
-              L ${w0-10} ${h1-10-h2}
-              L ${w0-20} ${h1-10-h2}
-              L ${w0-20} ${h1-40-h2}
-              L ${w0-10} ${h1-40-h2}
-              L ${w0-10} ${h1-30-h2}
-              L ${w0-50} ${h1-30-h2}
-              L ${w0-50} ${h1-10-h2}
-              L ${50} ${h1-10-h2}
-              L ${50} ${h1-30-h2}
-              L ${10} ${h1-30-h2}
-              L ${10} ${h1-40-h2}
-              L ${20} ${h1-40-h2}
-              L ${20} ${h1-10-h2}
-              L ${10} ${h1-10-h2}
-              L ${10} ${h1-20-h2}
-              L ${40} ${h1-20-h2}
-              L ${40} ${h1-10-h2}
-              L ${30} ${h1-10-h2}
-              L ${30} ${h1-50-h2}
-              L ${10} ${h1-50-h2}
-              L 10 100"
-            stroke="#64FFDA"
-            stroke-width="2"
-            
-            fill="none"
-                    />
-                
-                </svg>
-                ''',
-              width: w0,
-              height: h1,
-            ),
+          child: AnimatedBuilder(
+              animation: _pathAnimation,
+              builder: (context, child) {
+                return CustomPaint(
+                  painter: BorderPainter(_pathAnimation.value),
+                  size: Size(w0, h1),
+                );
+              },
           ),
         ),
       ),
     );
   }
+}
+
+class BorderPainter extends CustomPainter {
+  final double progress;
+  final Paint _paint;
+
+  BorderPainter(this.progress) : _paint = Paint()
+    ..color = const Color(0xFF64FFDA)
+    ..strokeWidth = 2
+    ..style = PaintingStyle.stroke;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    double w0 = size.width;
+    double h1 = size.height;
+    double h2 = size.height * 0.1;
+
+    var path = Path();
+    path.moveTo(10, 100);
+    path.lineTo(10, 50);
+    path.lineTo(30, 50);
+    path.lineTo(30, 10);
+    path.lineTo(40, 10);
+    path.lineTo(40, 20);
+    path.lineTo(10, 20);
+    path.lineTo(10, 10);
+    path.lineTo(20, 10);
+    path.lineTo(20, 40);
+    path.lineTo(10, 40);
+    path.lineTo(10, 30);
+    path.lineTo(50, 30);
+    path.lineTo(50, 10);
+    path.lineTo(w0 - 50, 10);
+    path.lineTo(w0 - 50, 30);
+    path.lineTo(w0 - 10, 30);
+    path.lineTo(w0 - 10, 40);
+    path.lineTo(w0 - 20, 40);
+    path.lineTo(w0 - 20, 10);
+    path.lineTo(w0 - 10, 10);
+    path.lineTo(w0 - 10, 20);
+    path.lineTo(w0 - 40, 20);
+    path.lineTo(w0 - 40, 10);
+    path.lineTo(w0 - 30, 10);
+    path.lineTo(w0 - 30, 50);
+    path.lineTo(w0 - 10, 50);
+    path.lineTo(w0 - 10, h1 - 50 - h2);
+    path.lineTo(w0 - 30, h1 - 50 - h2);
+    path.lineTo(w0 - 30, h1 - 10 - h2);
+    path.lineTo(w0 - 40, h1 - 10 - h2);
+    path.lineTo(w0 - 40, h1 - 20 - h2);
+    path.lineTo(w0 - 10, h1 - 20 - h2);
+    path.lineTo(w0 - 10, h1 - 10 - h2);
+    path.lineTo(w0 - 20, h1 - 10 - h2);
+    path.lineTo(w0 - 20, h1 - 40 - h2);
+    path.lineTo(w0 - 10, h1 - 40 - h2);
+    path.lineTo(w0 - 10, h1 - 30 - h2);
+    path.lineTo(w0 - 50, h1 - 30 - h2);
+    path.lineTo(w0 - 50, h1 - 10 - h2);
+    path.lineTo(50, h1 - 10 - h2);
+    path.lineTo(50, h1 - 30 - h2);
+    path.lineTo(10, h1 - 30 - h2);
+    path.lineTo(10, h1 - 40 - h2);
+    path.lineTo(20, h1 - 40 - h2);
+    path.lineTo(20, h1 - 10 - h2);
+    path.lineTo(10, h1 - 10 - h2);
+    path.lineTo(10, h1 - 20 - h2);
+    path.lineTo(40, h1 - 20 - h2);
+    path.lineTo(40, h1 - 10 - h2);
+    path.lineTo(30, h1 - 10 - h2);
+    path.lineTo(30, h1 - 50 - h2);
+    path.lineTo(10, h1 - 50 - h2);
+    path.lineTo(10, 100);
+
+    PathMetrics pathMetrics = path.computeMetrics();
+    for (PathMetric pathMetric in pathMetrics) {
+      Path extractPath = pathMetric.extractPath(
+        0.0,
+        pathMetric.length * progress,
+      );
+      canvas.drawPath(extractPath, _paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(BorderPainter oldDelegate) =>
+      oldDelegate.progress != progress;
 }
