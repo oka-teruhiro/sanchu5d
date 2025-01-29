21cb22559fe11c6de998eccfae24b27b407bf656
oka-teruhiro <oka331@gmail.com>
2025-01-29 19:50:05 +0900

    26: 6.3.2 SvgPicture.string()で枠飾りを追加

--- lib/widgets/omikuji_bottom_sheet.dart
+++ lib/widgets/omikuji_bottom_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 // 画面下からおみくじの結果が出てくる
 class OmikujiBottomSheet extends StatefulWidget {
@@ -47,6 +48,8 @@ class _OmikujiBottomSheetState extends State<OmikujiBottomSheet>
     double w0 = MediaQuery.of(context).size.width;
     double h0 = MediaQuery.of(context).size.height;
     double h1 = h0 * 0.9;
+    double h2 = h0 * 0.1;
+
 
     return FadeTransition( // フェードインさせる
       opacity: _animation,
@@ -58,7 +61,76 @@ class _OmikujiBottomSheetState extends State<OmikujiBottomSheet>
           height: h1,
           child: Container(
             color: Colors.blueGrey,
-            child: Text('おみくじ'),
+            child: SvgPicture.string(
+              '''
+      <svg width= w0 height= h1 viewBox="0 0 $w0 $h1">
+        
+        <path
+            d="M 10 100
+              L 10 50
+              L 30 50
+              L 30 10
+              L 40 10
+              L 40 20
+              L 10 20
+              L 10 10
+              L 20 10
+              L 20 40
+              L 10 40
+              L 10 30
+              L 50 30
+              L 50 10
+              L ${w0-50} 10
+              L ${w0-50} 30
+              L ${w0-10} 30
+              L ${w0-10} 40
+              L ${w0-20} 40
+              L ${w0-20} 10
+              L ${w0-10} 10
+              L ${w0-10} 20
+              L ${w0-40} 20
+              L ${w0-40} 10
+              L ${w0-30} 10
+              L ${w0-30} 50
+              L ${w0-10} 50
+              L ${w0-10} ${h1-50-h2}
+              L ${w0-30} ${h1-50-h2}
+              L ${w0-30} ${h1-10-h2}
+              L ${w0-40} ${h1-10-h2}
+              L ${w0-40} ${h1-20-h2}
+              L ${w0-10} ${h1-20-h2}
+              L ${w0-10} ${h1-10-h2}
+              L ${w0-20} ${h1-10-h2}
+              L ${w0-20} ${h1-40-h2}
+              L ${w0-10} ${h1-40-h2}
+              L ${w0-10} ${h1-30-h2}
+              L ${w0-50} ${h1-30-h2}
+              L ${w0-50} ${h1-10-h2}
+              L ${50} ${h1-10-h2}
+              L ${50} ${h1-30-h2}
+              L ${10} ${h1-30-h2}
+              L ${10} ${h1-40-h2}
+              L ${20} ${h1-40-h2}
+              L ${20} ${h1-10-h2}
+              L ${10} ${h1-10-h2}
+              L ${10} ${h1-20-h2}
+              L ${40} ${h1-20-h2}
+              L ${40} ${h1-10-h2}
+              L ${30} ${h1-10-h2}
+              L ${30} ${h1-50-h2}
+              L ${10} ${h1-50-h2}
+              L 10 100"
+            stroke="#64FFDA"
+            stroke-width="2"
+            
+            fill="none"
+          />
+
+      </svg>
+      ''',
+              width: w0,
+              height: h1,
+            ),
           ),
         ),
       ),
