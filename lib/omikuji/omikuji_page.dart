3c98b2fdfcc66453b0f2006486e62c5bbddef486
oka-teruhiro <oka331@gmail.com>
2025-02-11 10:06:23 +0900

    40: 6.3.18 おみくじ使用法をほぼ実装

--- lib/omikuji/omikuji_page.dart
+++ lib/omikuji/omikuji_page.dart
@@ -1,8 +1,8 @@
-
 import 'dart:math';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:sanchu5d/omikuji/omikuji_siyouhou.dart';
 
 import 'omikuji_bottom_sheet.dart';
 import 'omikuji_service.dart';
@@ -15,9 +15,9 @@ class OmikujiPage extends StatefulWidget {
 }
 
 class _OmikujiPageState extends State<OmikujiPage> {
-
   final OmikujiService _omikujiService = OmikujiService();
-  late final  List<bool> _listExpanded = [false, false, false, false, false];
+  late final List<bool> _listExpanded = [true, false, false, false, false];
+
   void _togglePanel(int index) {
     setState(() {
       if (index == 0) {
@@ -28,8 +28,10 @@ class _OmikujiPageState extends State<OmikujiPage> {
         _listExpanded[2] = !_listExpanded[2];
       } else if (index == 3) {
         _listExpanded[3] = !_listExpanded[3];
-      } else  {
+      } else if (index == 4) {
         _listExpanded[4] = !_listExpanded[4];
+      } else {
+        _listExpanded[5] = !_listExpanded[5];
       }
     });
   }
@@ -44,33 +46,200 @@ class _OmikujiPageState extends State<OmikujiPage> {
   Widget build(BuildContext context) {
     return MediaQuery(
       data: MediaQuery.of(context)
-          .copyWith(textScaler:  const TextScaler.linear(1.0)),
-        child: Scaffold(
-          backgroundColor: Colors.black,
-          appBar: AppBar(
-            backgroundColor: Colors.black12,
-            title: Text(
-                'おみくじ',
-                style: const TextStyle(
-                  color: Colors.pinkAccent,
-                  fontWeight: FontWeight.bold,
-                ),
+          .copyWith(textScaler: const TextScaler.linear(1.0)),
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Colors.black12,
+          title: Text(
+            'おみくじ（工事中）',
+            style: const TextStyle(
+              color: Colors.pinkAccent,
+              fontWeight: FontWeight.bold,
             ),
           ),
-          body: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                    'おみくじ機能の使い方',
-                  style: TextStyle(
-                    fontSize: 16,
-                  ),
-                ),Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Center(
-                    child: ElevatedButton(
+        ),
+        body: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: ListView(
+                children: [
+                  ExpansionPanelList(
+                    expansionCallback: (int panelIndex, bool isExpanded) {
+                      _togglePanel(panelIndex);
+                    },
+                    animationDuration: const Duration(seconds: 1),
+                    children: [
+                      // ToDo:■■■■■　使用方法　■■■■■
+                      ExpansionPanel(
+                        isExpanded: _listExpanded[0],
+                        headerBuilder:
+                            (BuildContext context, bool isExpanded) {
+                          return ListTile(
+                            title: Text(
+                              '使用方法',
+                              style: const TextStyle(
+                                color: Colors.greenAccent,
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                          );
+                        },
+                        body: Column(
+                          children: [
+                            omikujiSiyouhou,
+                            ListTile(
+                              trailing: const Icon(Icons.expand_less),
+                              onTap: () => _closePanel(0),
+                            ),
+                          ],
+                        ),
+                      ),
+                      // ToDo:■■■■■　東洋五術　■■■■■
+                      ExpansionPanel(
+                        isExpanded: _listExpanded[1],
+                        headerBuilder:
+                            (BuildContext context, bool isExpanded) {
+                          return ListTile(
+                            title: Text(
+                              '東洋五術からみた「おみくじ」',
+                              style: const TextStyle(
+                                color: Colors.greenAccent,
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                          );
+                        },
+                        body: Column(
+                          children: [
+                            ListTile(
+                              title: Text(
+                                'おみくじを引く占術は、東洋五術の卜術に分類されます。・・・',
+                                style: const TextStyle(
+                                  height: 1.5,
+                                ),
+                              ),
+                            ),
+                            ListTile(
+                              trailing: const Icon(Icons.expand_less),
+                              onTap: () => _closePanel(1),
+                            ),
+                          ],
+                        ),
+                      ),
+                      // ToDo:■■■■■　初詣　■■■■■
+                      ExpansionPanel(
+                        isExpanded: _listExpanded[2],
+                        headerBuilder:
+                            (BuildContext context, bool isExpanded) {
+                          return ListTile(
+                            title: Text(
+                              '初詣からみた「おみくじ」',
+                              style: const TextStyle(
+                                color: Colors.greenAccent,
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                          );
+                        },
+                        body: Column(
+                          children: [
+                            ListTile(
+                              title: Text(
+                                '初詣でおみくじを引くと思いますが、・・・',
+                                style: const TextStyle(
+                                  height: 1.5,
+                                ),
+                              ),
+                            ),
+                            ListTile(
+                              trailing: const Icon(Icons.expand_less),
+                              onTap: () => _closePanel(2),
+                            ),
+                          ],
+                        ),
+                      ),
+                      // ToDo:■■■■■　精誠　■■■■■
+                      ExpansionPanel(
+                        isExpanded: _listExpanded[3],
+                        headerBuilder:
+                            (BuildContext context, bool isExpanded) {
+                          return ListTile(
+                            title: Text(
+                              '略式としての初詣',
+                              style: const TextStyle(
+                                color: Colors.greenAccent,
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                          );
+                        },
+                        body: Column(
+                          children: [
+                            ListTile(
+                              title: Text(
+                                'なぜ、初詣の作法で恩恵が受けられるのかというと・・・',
+                                style: const TextStyle(
+                                  height: 1.5,
+                                ),
+                              ),
+                            ),
+                            ListTile(
+                              trailing: const Icon(Icons.expand_less),
+                              onTap: () => _closePanel(3),
+                            ),
+                          ],
+                        ),
+                      ),
+                      // ToDo:■■■■■　精誠　■■■■■
+                      ExpansionPanel(
+                        isExpanded: _listExpanded[4],
+                        headerBuilder:
+                            (BuildContext context, bool isExpanded) {
+                          return ListTile(
+                            title: Text(
+                              '「おみくじ」の精度を上げるには',
+                              style: const TextStyle(
+                                color: Colors.greenAccent,
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                          );
+                        },
+                        body: Column(
+                          children: [
+                            ListTile(
+                              title: Text(
+                                '以上のことから、より「おみくじ」の精度を上げるには、次のことをすることをおすすめします。・・・',
+                                style: const TextStyle(
+                                  height: 1.5,
+                                ),
+                              ),
+                            ),
+                            ListTile(
+                              trailing: const Icon(Icons.expand_less),
+                              onTap: () => _closePanel(4),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            const Divider(
+              color: Colors.blue,
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
                       style: ElevatedButton.styleFrom(
                         backgroundColor: Colors.tealAccent,
                       ),
@@ -88,155 +257,7 @@ class _OmikujiPageState extends State<OmikujiPage> {
                         //Navigator.pop(context);
                       },
                     ),
-                  ),
-                ),
-                const Divider(
-                  color: Colors.blue,
-                ),
-                Expanded(
-                    child: ListView(
-                      children: [
-                        ExpansionPanelList(
-                          expansionCallback: (int panelIndex, bool isExpanded) {
-                            _togglePanel(panelIndex);
-                          },
-                          animationDuration: const Duration(seconds: 1),
-                          children: [
-                            // ToDo:■■■■■　東洋五術　■■■■■
-                            ExpansionPanel(
-                              isExpanded: _listExpanded[0],
-                                headerBuilder: (BuildContext context, bool isExpanded) {
-                                return ListTile(
-                                  title: Text(
-                                      '東洋五術からみた「おみくじ」',
-                                    style: const TextStyle(
-                                      color: Colors.greenAccent,
-                                      fontWeight: FontWeight.normal,
-                                    ),
-                                  ),
-                                );
-                                },
-                                body: Column(
-                                  children: [
-                                    ListTile(
-                                      title: Text(
-                                          'おみくじを引く占術は、東洋五術の卜術に分類されます。・・・',
-                                        style: const TextStyle(
-                                          height: 1.5,
-                                        ),
-                                      ),
-                                    ),
-                                    ListTile(
-                                      trailing: const Icon(Icons.expand_less),
-                                      onTap: () => _closePanel(0),
-                                    ),
-                                  ],
-                                ),
-                            ),
-                            // ToDo:■■■■■　初詣　■■■■■
-                            ExpansionPanel(
-                              isExpanded: _listExpanded[1],
-                              headerBuilder: (BuildContext context, bool isExpanded) {
-                                return ListTile(
-                                  title: Text(
-                                    '初詣からみた「おみくじ」',
-                                    style: const TextStyle(
-                                      color: Colors.greenAccent,
-                                      fontWeight: FontWeight.normal,
-                                    ),
-                                  ),
-                                );
-                              },
-                              body: Column(
-                                children: [
-                                  ListTile(
-                                    title: Text(
-                                      '初詣でおみくじを引くと思いますが、・・・',
-                                      style: const TextStyle(
-                                        height: 1.5,
-                                      ),
-                                    ),
-                                  ),
-                                  ListTile(
-                                    trailing: const Icon(Icons.expand_less),
-                                    onTap: () => _closePanel(1),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            // ToDo:■■■■■　精誠　■■■■■
-                            ExpansionPanel(
-                              isExpanded: _listExpanded[2],
-                              headerBuilder: (BuildContext context, bool isExpanded) {
-                                return ListTile(
-                                  title: Text(
-                                    '略式としての初詣',
-                                    style: const TextStyle(
-                                      color: Colors.greenAccent,
-                                      fontWeight: FontWeight.normal,
-                                    ),
-                                  ),
-                                );
-                              },
-                              body: Column(
-                                children: [
-                                  ListTile(
-                                    title: Text(
-                                      'なぜ、初詣の作法で恩恵が受けられるのかというと・・・',
-                                      style: const TextStyle(
-                                        height: 1.5,
-                                      ),
-                                    ),
-                                  ),
-                                  ListTile(
-                                    trailing: const Icon(Icons.expand_less),
-                                    onTap: () => _closePanel(2),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            // ToDo:■■■■■　精誠　■■■■■
-                            ExpansionPanel(
-                              isExpanded: _listExpanded[3],
-                              headerBuilder: (BuildContext context, bool isExpanded) {
-                                return ListTile(
-                                  title: Text(
-                                    '「おみくじ」の精度を上げるには',
-                                    style: const TextStyle(
-                                      color: Colors.greenAccent,
-                                      fontWeight: FontWeight.normal,
-                                    ),
-                                  ),
-                                );
-                              },
-                              body: Column(
-                                children: [
-                                  ListTile(
-                                    title: Text(
-                                      '以上のことから、より「おみくじ」の精度を上げるには、次のことをすることをおすすめします。・・・',
-                                      style: const TextStyle(
-                                        height: 1.5,
-                                      ),
-                                    ),
-                                  ),
-                                  ListTile(
-                                    trailing: const Icon(Icons.expand_less),
-                                    onTap: () => _closePanel(3),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
-                ),
-                const Divider(
-                  color: Colors.blue,
-                ),Center(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ElevatedButton(
+                    ElevatedButton(
                       style: ElevatedButton.styleFrom(
                         backgroundColor: Colors.tealAccent,
                       ),
@@ -252,13 +273,13 @@ class _OmikujiPageState extends State<OmikujiPage> {
                         Navigator.pop(context);
                       },
                     ),
-                  ),
+                  ],
                 ),
-
-              ],
+              ),
             ),
-          ),
+          ],
         ),
+      ),
     );
   }
 
